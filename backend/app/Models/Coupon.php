dab07d348a531d083a2186e406e5bdccbd718860
Your Name <enggarrahayu64@gmail.com>
2024-09-26 00:18:43 +0700

    Coupon CRUD for admins

--- backend/app/Models/Coupon.php
+++ backend/app/Models/Coupon.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Coupon extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = Str::upper($value);
+    }
+
+    public function checkIfValid()
+    {
+        if ($this->valid_until > Carbon::now()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
